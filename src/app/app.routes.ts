c04867aed51a72f1ad1907d157a26036b321c53d
AL <amandarae220>
2026-04-09 23:31:10 -0400

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { SetupComponent } from './pages/setup/setup.component';
-import { GameComponent } from './pages/game/game.component';
-
-export const routes: Routes = [
-  { path: '', component: SetupComponent },
-  { path: 'game', component: GameComponent }
-];
